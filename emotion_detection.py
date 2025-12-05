c3edc0c4a0f262e7384c943538ee80c7a36a49c2
badr-saud <alrhbyb387@gmail.com>
2025-12-05 09:07:14 -0500

    Task 2: fromatted output

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,19 +2,26 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
-    # Endpoint URL
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
-    # Required headers
     headers = {
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
     }
-    # JSON payload
     payload = {
         "raw_document": {
             "text": text_to_analyze
         }
     }
-    # POST request
     response = requests.post(url, headers=headers, json=payload)
-    # Return the text attribute of the response object
-    return response.text
+    data = json.loads(response.text)
+    emotions = data["emotionPredictions"][0]["emotion"]
+    dominant = max(emotions, key=emotions.get)
+    result = {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant
+    }
+
+    return result
